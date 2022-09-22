c41838f0c4b73ff00c06c8ee626c1a26ac37e129
Balamanikandan <balamanikandan.balasubramanian@gmail.com>
2022-09-22 09:58:14 +0530

    Helloworld.java

--- Test/src/main/java/org/example/HelloWorld.java
+++ Test/src/main/java/org/example/HelloWorld.java
@@ -1,6 +1,6 @@
 package org.example;
 
-public class HelloWorld {
+public class HelloWorld { //sample to print hello..
     public static void main(String... args) {
         System.out.println("Hello world!");
     }
